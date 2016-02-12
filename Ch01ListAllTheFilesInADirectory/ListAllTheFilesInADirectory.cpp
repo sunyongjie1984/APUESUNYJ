97103aeebae7ee379979a15f8c71985db635aa67
Jack SUN <sunyongjie1984@gmail.com>
2016-02-12 22:45:39 +0800

    modify

--- Ch01ListAllTheFilesInADirectory/ListAllTheFilesInADirectory.cpp
+++ Ch01ListAllTheFilesInADirectory/ListAllTheFilesInADirectory.cpp
@@ -1,24 +1,25 @@
 // 1.3ListAllTheFilesInADirectory.cpp
+
 #include <dirent.h>
 
 #include "../lib/apuesunyj.h"
 
 int main(int argc, char *argv[])
 {
-	DIR* dp;
-	struct dirent* dirp;
-	if (2 != argc)
-	{
-		err_quit("usage: ls directory_name");
-	}
-	if ((dp = opendir(argv[1])) == NULL)
-	{
-		err_sys("can’t open %s", argv[1]);
-	}
-	while ((dirp = readdir(dp)) != NULL)
-	{
-		printf("%s\n", dirp->d_name);
-	}
-	closedir(dp);
-	return 0;
+    DIR* dp;
+    struct dirent* dirp;
+    if (2 != argc)
+    {
+        err_quit("usage: ls directory_name");
+    }
+    if ((dp = opendir(argv[1])) == NULL)
+    {
+        err_sys("can’t open %s", argv[1]);
+    }
+    while ((dirp = readdir(dp)) != NULL)
+    {
+        printf("%s\n", dirp->d_name);
+    }
+    closedir(dp);
+    return 0;
 }
